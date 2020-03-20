dc83c58d611f859cd825bb01d24f3d79033c2beb
Héctor Romero Lopez <cyberh99@gmail.com>
2020-03-20 14:13:42 +0100

    Noitifications if server is down with 5 minutes of cron

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from app import slack_client
 from app import WebActions
+import schedule
+import time
+import sys
 
-if __name__ == "__main__":
-    status = WebActions("http://54.159.185.30:8000/").check_website_status()
+
+def testing_web(url: str):
+    print(f"[?] Testing {url}")
+    status = WebActions(url).check_website_status()
     if status is not True:
         slack_client.chat_postMessage(
             channel="#servernotifications",
             text=status
         )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py url")
+        exit(-1)
+
+    schedule.every(5).minutes.do(testing_web, (sys.argv[1]))
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
