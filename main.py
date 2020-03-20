4a2acf5591de9e7f2b8596ab540e9af72402d344
Héctor Romero Lopez <cyberh99@gmail.com>
2020-03-20 12:29:36 +0100

    Added correct main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from app import slack_client
 from app import WebActions
 
 if __name__ == "__main__":
-    if WebActions("http://54.159.185.30:8000/").check_website_status() is not True:
+    status = WebActions("http://54.159.185.30:8000/").check_website_status()
+    if status is not True:
         slack_client.chat_postMessage(
             channel="#servernotifications",
-            text="[!] Notificacion de prueba"
+            text=status
         )
